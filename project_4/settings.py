fbfb7d8b253b0a66d35515350451f3c0e2ae5341
afnasamadpk <thasleena@gmail.com>
2021-02-17 14:29:29 +0530

    ddddd

--- project_4/settings.py
+++ project_4/settings.py
@@ -77,12 +77,27 @@ WSGI_APPLICATION = 'project_4.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+if DEBUG:
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME':  'afna1996$db1',
+            'USER': 'afna1996',
+            'PASSWORD': 'thasli@1998',
+            'HOST': 'afna1996.mysql.pythonanywhere-services.com',
+            'PORT': 3306
+        }
+    }
 
 
 # Password validation
